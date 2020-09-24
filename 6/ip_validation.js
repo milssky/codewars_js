877c89a78bb3c29cf019608067b30d3706f50976
milssky <2furius@gmail.com>
2020-09-24 12:03:59 +0300

    ip validations

---
+++ 6/ip_validation.js
@@ -0,0 +1,14 @@
+function isValidIP(str) {
+    let data = str.split('.');
+    if (data.length !== 4) return false;
+    for (var i = 0; i < data.length; i++) {
+        if (!(data[i] >= 0 && data[i] <= 255)) return false;
+        if (!data[i][0]) return false
+    }
+    return true
+}
+
+//best solution
+function isValidIP(str) {
+    return /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/.test(str);
+}
